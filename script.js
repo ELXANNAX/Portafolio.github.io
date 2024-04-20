04a7e827beb581f8c233793856bb57fd0de91f4d
Oscar Albarracín <albarracinjaimesoscarjose@gmail.com>
2024-04-19 19:26:08 -0500

    docs(Nuevos estilos): :lipstick: Hemos añadido nuevos estilos
    Hemos arreglado la transición entre las sección, dado un aspecto a la barra de scroll

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll('nav a, a');
+
+    links.forEach(link => {
+      link.addEventListener('click', smoothScroll);
+    });
+
+    function smoothScroll(e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href').substring(1);
+      const target = document.getElementById(targetId);
+
+      window.scroll({
+        top: target.offsetTop,
+        behavior: 'smooth'
+      });
+    }
+  });
